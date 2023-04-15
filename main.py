1255dd44df873e75196f225f25dfa1058a72cb93
vijayaraj <vijayarajsuyambu@gmail.com>
2023-04-16 02:45:44 +0400

    update the job detail table status_id based upon the company,designation coming from json_later json file (if matched).
    Important:
    Above match based upon the time interval between 25 days.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ dir_path_for_Job_Status_Later = "/Users/web_dev/Desktop/JOB_Status/"
 db_name = 'JOB_Analyser_DB'
 tbl_name_Job_Status_Later = 'JOB_Analyser_App_jobstatuslater'
 
+tbl_name_Job_detail = 'JOB_Analyser_App_jobdetail'
+
 insert_query_for_Job_Status_Later = '''INSERT INTO 
 JOB_Analyser_App_jobstatuslater(company,designation,status,statusid,source,created)
   VALUES (%s,%s,%s,%s,%s,%s)'''
@@ -38,17 +40,49 @@ def delete_duplicate_records(cursor12):
         return False
 
 
-def saving_in_db(cursor1, query, query_values):
+def saving_in_db(cursor1, query, query_values, txt_task):
     try:
         cursor1.execute(query, query_values)
         db.commit()
-        print("Saved in Database Successfully")
+        print(txt_task + " Database Successfully")
         return True
     except mysql.Error as err:
         print("MySql Exception: {}".format(err))
         return False
     except Exception as eee:
         print("Exception: {}".format(eee))
+
+
+def updating_in_db(cursor1, query, txt_task):
+    try:
+        cursor1.execute(query)
+        db.commit()
+        print(txt_task + " Database Successfully")
+        return True
+    except mysql.Error as err:
+        print("MySql Exception: {}".format(err))
+        return False
+    except Exception as eee:
+        print("Exception: {}".format(eee))
+
+
+def update_job_detail_record(cursor1, arr_json_data, file_name):
+    for r_data in arr_json_data:
+        try:
+            company = r_data['Company'].strip()
+            designation = r_data['Designation'].strip()
+            if (company == "") or (designation == ""):
+                print("Company or Designation is empty")
+                return False
+            statusid = str(r_data['StatusID'])
+
+            update_query_for_job_detail = "UPDATE JOB_Analyser_DB.JOB_Analyser_App_jobdetail SET status_id_id = " + statusid + " WHERE company = '" + company + "' AND designation = '" + designation + "' AND created > (curdate() - interval 25 day)"
+
+            # print(update_query_for_job_detail)
+            updating_in_db(cursor1, update_query_for_job_detail, 'Updated')
+
+        except Exception as eee:
+            print("General Exception in METHOD update_job_detail_record filename " + file_name + " " + str(eee))
         # return False
 
 
@@ -67,13 +101,37 @@ def add_new_job_status_later_record(cursor1, arr_json_data, file_name):
             # if not saving_in_db(cursor1, insert_query_for_Job_Status_Later, jb_status_later):
             #     return False
 
-            saving_in_db(cursor1, insert_query_for_Job_Status_Later, jb_status_later)
+            saving_in_db(cursor1, insert_query_for_Job_Status_Later, jb_status_later, 'Saved')
 
         except Exception as eee:
             print("General Exception in filename " + file_name + " " + str(eee))
             # return False
 
     return True
+
+
+def read_job_status_later_jsonForUpdate(cursor11):
+    try:
+        # print(dir_path)
+        for file_name in [file for file in os.listdir(dir_path_for_Job_Status_Later) if
+                          (file.endswith('.json') and not (file.__contains__('done_')))]:
+            path_with_file_name = dir_path_for_Job_Status_Later + file_name
+            try:
+                with open(path_with_file_name) as json_file:
+                    data = json.load(json_file)
+
+                    update_job_detail_record(cursor11, data, file_name)
+
+            except EnvironmentError:
+                print('EnvironmentError Exception')
+                continue
+    except FileNotFoundError:
+        print("Error: FileNotFoundError")
+    except EOFError:
+        print("Exception")
+    except Exception:
+        print('General Exception in function name: read_job_status_later_json  ' + str(Exception))
+        print(Exception)
 
 
 def read_job_status_later_json(cursor11):
@@ -123,7 +181,8 @@ def mysql_connect():
 if __name__ == '__main__':
     db = mysql_connect()
     cursor = db.cursor()
-    delete_duplicate_records(cursor)
+    # delete_duplicate_records(cursor)
+    read_job_status_later_jsonForUpdate(cursor)
     read_job_status_later_json(cursor)
     delete_duplicate_records(cursor)
     db.close()
